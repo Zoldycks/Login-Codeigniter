faf6c819203a0d3bfc9c5dc1dbab2f6798527268
Yasha <alexander_leu@hotmail.com>
2013-12-17 10:54:43 -0500

    Agregando Login
    Login para el panel de ADMINISTRACION

---
+++ application/controllers/login.php
@@ -0,0 +1,92 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Created by JetBrains PhpStorm.
+ * User: isra
+ * Date: 19/01/13
+ * Time: 18:51
+ * To change this template use File | Settings | File Templates.
+ */
+class Login extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+		$this->load->model('login_model');
+		$this->load->library(array('session','form_validation'));
+		$this->load->helper(array('url','form'));
+		$this->load->database('default');
+    }
+	
+	public function index()
+	{	
+		switch ($this->session->userdata('perfil')) {
+			case '':
+				$data['token'] = $this->token();
+				$data['titulo'] = 'Login con roles de usuario en codeigniter';
+				$this->load->view('login_view',$data);
+				break;
+			case 'administrador':
+				redirect(base_url().'admin');
+				break;
+			case 'editor':
+				redirect(base_url().'editor');
+				break;	
+			case 'suscriptor':
+				redirect(base_url().'suscriptor');
+				break;
+			default:		
+				$data['titulo'] = 'Login con roles de usuario en codeigniter';
+				$this->load->view('login_view',$data);
+				break;		
+		}
+	}
+	
+	public function token()
+	{
+		$token = md5(uniqid(rand(),true));
+		$this->session->set_userdata('token',$token);
+		return $token;
+	}
+	
+	public function new_user()
+	{
+		if($this->input->post('token') && $this->input->post('token') == $this->session->userdata('token'))
+		{
+            $this->form_validation->set_rules('username', 'nombre de usuario', 'required|trim|min_length[2]|max_length[150]|xss_clean');
+            $this->form_validation->set_rules('password', 'password', 'required|trim|min_length[6]|max_length[150]|xss_clean');
+ 
+            //lanzamos mensajes de error si es que los hay
+            $this->form_validation->set_message('required', 'El %s es requerido');
+            $this->form_validation->set_message('min_length', 'El %s debe tener al menos %s carácteres');
+            $this->form_validation->set_message('max_length', 'El %s debe tener al menos %s carácteres');
+			if($this->form_validation->run() == FALSE)
+			{
+				$this->index();
+			}else{
+				$username = $this->input->post('username');
+				$password = sha1($this->input->post('password'));
+				$check_user = $this->login_model->login_user($username,$password);
+				if($check_user == TRUE)
+				{
+					$data = array(
+	                'is_logued_in' 	=> 		TRUE,
+	                'id_usuario' 	=> 		$check_user->id,
+	                'perfil'		=>		$check_user->perfil,
+	                'username' 		=> 		$check_user->username,
+	                'email' 		=> 		$check_user->email
+            		);		
+					$this->session->set_userdata($data);
+					$this->index();
+				}
+			}
+		}else{
+			redirect(base_url().'login');
+		}
+	}
+
+	public function logout_ci()
+	{
+		$this->session->sess_destroy();
+		$this->index();
+	}
+}
---
+++ roles_codeigniter.sql
@@ -0,0 +1,50 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generaciÃ³n: 20-01-2013 a las 00:22:17
+-- VersiÃ³n del servidor: 5.5.16
+-- VersiÃ³n de PHP: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `roles_codeigniter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `perfil` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `perfil`, `username`, `password`) VALUES
+(1, 'administrador', 'israel965', 'd033e22ae348aeb5660fc2140aec35850c4da997'),
+(2, 'editor', 'vanessa78', 'ab41949825606da179db7c89ddcedcc167b64847'),
+(3, 'suscriptor', 'jaime70', '1a248d7a471ad8d5993aa523c8397ce1d0bafe78');
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
